6b62d661fc80459e4d3b53fb84e98f0e6ec24634
Guillaume Paumier <gp@guillaumepaumier.com>
2015-06-23 15:17:41 -0700

    Basic proof of concept to get a list of wikis

---
+++ getWikis.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Get a set of wikis based on a few parameters.
+// Extracts the list of wikis from the Wikimedia Tool Labs metadata database (meta_p), and returns information about those wikis, to be used in another program; for example, in a program that iterates over a selection of wikis to perform a global analysis.
+
+
+// Modules
+
+var mysql = require('mysql');
+
+
+// Local files
+
+var config = require('./config');
+
+
+// Set up MySQL connection
+
+// The metadata database is available in all slices, so we use the alias for a small wiki, which is expected to be under the less load than the slices used for bigger wikis.
+
+var connection = mysql.createConnection({
+    host: 'kgwiki.labsdb',
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: 'meta_p'
+});
+
+
+// Build the query
+// TODO later: make the query more specific based on the parameters given by the user (e.g. exclude closed wiki, only return wikis with VisualEditor, etc.)
+
+var query = 'SELECT * FROM wiki;';
+
+function getWikis(){
+
+    connection.connect(function(err) {
+        if (err) throw err;
+    });
+
+    connection.query(query, function(err, rows, fields){
+        if (err) throw err;
+        console.log('Received ' + rows.length + ' wikis.');
+    });
+
+    connection.end(function(err) {
+        if (err) throw err;
+    });
+}
+
+getWikis();
