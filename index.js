7581eb2e84e2c5cd302e40f8d635e5f64bb56e79
Guillaume Paumier <gp@guillaumepaumier.com>
2015-06-24 19:48:05 -0700

    Add basic query to get sitelinks from Wikidata
    Query Wikidata with a list of item IDs, and extract sitelinks from the response for each item.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // Modules
 
 var mw = require('nodemw');
+var wdk = require('wikidata-sdk');
+var request = require('request');
 
 //
 
@@ -33,7 +35,7 @@ function getCategoryInfo(wiki, titles) {
 // Test query;
 // API sandbox: https://commons.wikimedia.org/wiki/Special:ApiSandbox#action=query&prop=categoryinfo&format=json&titles=Category%3ACC-BY-4.0
 
-getCategoryInfo(commonswiki, 'Category:CC-BY-4.0');
+//getCategoryInfo(commonswiki, 'Category:CC-BY-4.0');
 
 
 function getSiteStatistics(wiki) {
@@ -55,4 +57,23 @@ function getSiteStatistics(wiki) {
 // Test query; /w/api.php?action=query&meta=siteinfo&format=json&siprop=statistics
 // API sandbox: https://commons.wikimedia.org/wiki/Special:ApiSandbox#action=query&prop=categoryinfo&format=json&titles=Category%3ACC-BY-4.0
 
-getSiteStatistics(commonswiki);
+//getSiteStatistics(commonswiki);
+
+
+// Get site links from Wikidata for given items
+
+function getWikidataSitelinksByIDs(items) {
+
+    var wikidataQueryURL = wdk.getEntities(items, 'en', 'sitelinks', 'json');
+
+    request(wikidataQueryURL, function(err, response) {
+        var entities = JSON.parse(response.body).entities;
+
+        for (var item of items) {
+            console.log(entities[item].sitelinks);
+        };
+
+    });
+}
+
+getWikidataSitelinksByIDs(['Q6310062']);
